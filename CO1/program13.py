47a80a12d9be68cdcbb6fe3944aaf7e8f24b6d9d
Arjunk107 <69452657+Arjunk107@users.noreply.github.com>
2022-01-10 19:29:47 +0530

    CO1 PYTHON PROGRAM
    Create a list of colors from comma-seperated color names entered by user.Display first and last color

--- CO1/program13.py
+++ CO1/program13.py
@@ -1,3 +1,6 @@
+#Create a list of colors from comma-seperated color names entered by user.Display first and last color
+
+
 colours = input("enter the colours:")
 print(colours.split(","))
 print("first colour:", colours.split(",")[0])
