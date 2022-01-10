be03c6bb3d0930ffe216dd6f2b31c68749c72f02
Arjunk107 <69452657+Arjunk107@users.noreply.github.com>
2022-01-09 19:53:36 -1000

    CO1 PYTHON PROGRAM

--- CO1/program18.py
+++ CO1/program18.py
@@ -1,6 +1,5 @@
-# Merge two dictionaries
 
 dict1={"Anas":1,"Vishnu":18,"Vyshnav":17,"Akshay":5}
-dict2={"Lijas":7,"Murshid":19,"Nideesh":2,"Niranjan":8,"Gokul":12}
+dict2={"Abin":7,"Kiran":21,"Ajay":2,"Aswin":8,"Sudev":12,"Rahul":1,"Amal":18,"Nideesh":17,"Niranjan":5}
 dict1.update(dict2)
 print("Merged : ",dict1)
