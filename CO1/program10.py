a1ad921ecf31b0d4b1d903837e1f6c57dc05aa2b
Arjunk107 <69452657+Arjunk107@users.noreply.github.com>
2022-01-10 19:27:04 +0530

    CO1 PYTHON PROGRAM
    Accept the radius from user and find area of circle

--- CO1/program10.py
+++ CO1/program10.py
@@ -1,5 +1,5 @@
+# Accept the radius from user and find area of circle
 
-
-radius=int(input("Enter the radius:"))
-area=3.14*(radius*radius)
+rad=int(input("Enter the radius:"))
+area=3.14*(rad*rad)
 print("area:",area)
