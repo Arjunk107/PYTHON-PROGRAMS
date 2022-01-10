866148489df957b4dce4a0462d40d7033b767a4c
Arjunk107 <69452657+Arjunk107@users.noreply.github.com>
2022-01-09 19:51:36 -1000

    CO1 PYTHON PROGRAM

--- CO1/program17.py
+++ CO1/program17.py
@@ -1,6 +1,4 @@
-#Sort dictionary in ascending and descending order
-
-dict={"Aswin":7,"Vyshnav":21,"Lijas":2,"Binshad":8,"Murshid":12,"Gokul":1,"Vishnu":18,"Nideesh":17,"Niranjan":5}
+dict={"Abin":7,"Kiran":21,"Ajay":2,"Aswin":8,"Sudev":12,"Rahul":1,"Amal":18,"Nideesh":17,"Niranjan":5}
 l1=list(dict.items())
 l1.sort()
 print("Dictionary in ascending order : ",l1)
